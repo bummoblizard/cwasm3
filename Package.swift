9019615404ebc30bda41ba263a16d50d64fb3e9f
Ken Chung <ken.chung@thebaselab.com>
2022-04-09 17:55:14 +0800

    Link with ios_system

--- Package.swift
+++ Package.swift
@@ -9,14 +9,18 @@ let package = Package(
             targets: ["CWasm3"]
         ),
     ],
-    dependencies: [],
+    dependencies: [
+        .package(name: "ios_system", url: "https://github.com/holzschu/ios_system", from: "3.0.0")
+    ],
     targets: [
         .target(
             name: "CWasm3",
             cSettings: [
                 .define("APPLICATION_EXTENSION_API_ONLY", to: "YES"),
                 .define("d_m3MaxDuplicateFunctionImpl", to: "10"),
-            ]),
+            ],
+            linkerSettings: [.linkedFramework("ios_system")]
+        ),
         .testTarget(
             name: "CWasm3Tests",
             dependencies: ["CWasm3"],
